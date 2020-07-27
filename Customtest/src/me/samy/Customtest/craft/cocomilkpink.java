bea389fdf1e4e766f6a335e2d6b260fdb418eef5
BuildTools <unconfigured@null.spigotmc.org>
2020-07-27 16:39:20 +0100

    Test For Res

---
+++ Customtest/src/me/samy/Customtest/craft/cocomilkpink.java
@@ -0,0 +1,28 @@
+package me.samy.Customtest.craft;
+
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.ShapelessRecipe;
+import org.bukkit.plugin.Plugin;
+
+import me.samy.Customtest.test;
+
+public class cocomilkpink {
+
+	
+	 
+	
+	private static Plugin plugin = test.getPlugin(test.class);
+	public static ShapelessRecipe Pinkcoco() {
+		
+		ShapelessRecipe Pinkcoco = new ShapelessRecipe(new NamespacedKey(plugin, "Pink Bottle"), test.getEmptyPinkBottlecocoMilk());
+		Pinkcoco.addIngredient(test.getEmptyPinkBottleMilk().getData());
+		Pinkcoco.addIngredient(Material.COCOA_BEANS);
+		
+		return Pinkcoco;
+		
+		
+	}
+
+
+}
